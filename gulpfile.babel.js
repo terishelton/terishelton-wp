f016b253516430f1386ede00e004439d5585dec0
Teri Shelton <39071165+terishelton@users.noreply.github.com>
2021-09-07 19:48:33 -0700

    updated packages and tested gulp

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,27 +1,67 @@
-'use strict';
-
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
+import gulp from 'gulp';
+import autoprefixer from 'autoprefixer';
+// import babel from 'gulp-babel';
+// import browserSync from 'browser-sync';
+// import combine from 'gulp-scss-combine';
+import concat from 'gulp-concat';
+import cssnano from 'gulp-cssnano';
+// import eslint from 'gulp-eslint';
+import postcss from 'gulp-postcss';
+import print from 'gulp-print';
+import sass from 'gulp-sass';
+// import uglify from 'gulp-uglify';
 
 const paths = {
-	styles: {
+	publicStyles: {
 		src: 'sass/**/*.scss',
 		dest: 'sass'
 	}
 };
 
-function buildStyles() {
-	return gulp.src(paths.styles.src)
-		.pipe(sass.sync({outputStyle: 'compressed'}).on('error', sass.logError))
-		.pipe(gulp.dest('/style.css'));
-};
+// Initialize Browser Sync
+// const server = browserSync.create();
 
-exports.buildStyles = buildStyles;
-exports.watch = function () {
-	gulp.watch(paths.styles.src, ['sass']);
-};
+// function reload(done) {
+// 	server.reload();
+// 	done();
+// }
 
-const build = gulp.series(clean, gulp.parallel(styles, scripts));
-exports.build = build;
+// function serve(done) {
+// 	server.init({
+// 		proxy: '#',
+// 		port: '8181'
+// 	});
+// 	done();
+// }
 
-exports.default = build;
+/*
+* You can also declare named functions and export them as tasks
+*/
+export function publicStyles() {
+	return gulp.src(paths.publicStyles.src)
+		.pipe(print())
+		.pipe(sass({
+			outputStyle: 'compresed',
+			precision: 3,
+			errLogToConsole: true
+		}).on('error', sass.logError))
+		.pipe(postcss([
+			autoprefixer,
+			cssnano
+		]))
+		//.pipe(combine())
+		.pipe(concat('style.css'))
+		.pipe(gulp.dest(paths.publicStyles.dest));
+}
+
+export function watch() {
+	gulp.watch(paths.publicStyles.src, gulp.series(publicStyles));
+}
+
+// Run serve on first load, which also watches
+const firstRun = gulp.series(publicStyles, watch);
+
+/**
+ * Run the whole thing.
+ */
+export default firstRun;
