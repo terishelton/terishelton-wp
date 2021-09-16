fb2c616a3d3bb371528bb6f7c96772ac56f36d28
Teri Shelton <39071165+terishelton@users.noreply.github.com>
2021-09-15 20:32:02 -0700

    updated copyright

--- footer.php
+++ footer.php
@@ -17,7 +17,7 @@
 		<div class="site-info">
 			<?php
 				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( '&copy; 2019 %1$s. All rights reserved.', 'terishelton' ), '<a href="https://terishelton.com">Teri Shelton</a>' );
+				printf( esc_html__( '&copy; 2018-2021 %1$s. All rights reserved.', 'terishelton' ), '<a href="https://terishelton.com">Teri Shelton</a>' );
 			?>
 			| <a href="/blog/">Blog</a>
 		</div><!-- .site-info -->
